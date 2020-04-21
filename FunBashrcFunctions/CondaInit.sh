e736ef541e1879421cf7f55167f37874a6feadce
SaraMWillis <sarawillis@email.arizona.edu>
2020-04-21 15:33:11 -0700

    Create CondaInit.sh

---
+++ FunBashrcFunctions/CondaInit.sh
@@ -0,0 +1,48 @@
+# Conda initializations can cause a lot of woes in HPC and can affect
+# the functionality of things ranging from R installations to usage of 
+# OOD and even the ability to log in.
+# Because of this, I like to control when my conda environment is 
+# activated. This function is designed to suppress the activation
+# of anaconda until the user chooses by executing the command "xconda"
+
+# Location where anaconda is installed
+CONDAROOT=$HOME
+
+if [ $HOSTNAME = 'login2' ] || [ $HOSTNAME = 'login3' ]
+   then export CLUSTER=Ocelote
+elif [ $HOSTNAME = 'login' ]
+   then export CLUSTER=ElGato
+fi
+
+
+function xconda () {
+    export CONDA='True';
+    source ~/.bashrc
+}
+
+
+if [ -z "$CONDA" ]; then
+    :
+    if [ -z "$CLUSTER" ]; then
+        :
+    else
+        echo "Conda environment inactive. To activate use: xconda"
+    fi
+else
+    if [ $CONDA = 'True' ]; then
+        # >>> conda initialize >>>
+        # !! Contents within this block are managed by 'conda init' !!
+        __conda_setup="$('$CONDAROOT/anaconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+        if [ $? -eq 0 ]; then
+            eval "$__conda_setup"
+        else
+            if [ -f "$CONDAROOT/anaconda3/etc/profile.d/conda.sh" ]; then
+                . "$CONDAROOT/anaconda3/etc/profile.d/conda.sh" 
+            else
+                export PATH="$CONDAROOT/anaconda3/bin:$PATH"
+            fi
+        fi
+        unset __conda_setup
+        # <<< conda initialize <<<
+    fi
+fi
