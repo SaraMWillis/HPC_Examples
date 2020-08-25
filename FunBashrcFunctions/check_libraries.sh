512d8ce08d312db95bc76551f35840170758bde3
Sara <sarawillis@email.arizona.edu>
2020-08-25 09:49:07 -0700

    Update check_libraries.sh

--- FunBashrcFunctions/check_libraries.sh
+++ FunBashrcFunctions/check_libraries.sh
@@ -1,6 +1,6 @@
 #### Checks for available system libraries. Input: full or partial library names
 
-function library () {
+function library {
     message="Searching for system libraries matching keyword: $1"
     echo; echo $message
     num=${#message}
